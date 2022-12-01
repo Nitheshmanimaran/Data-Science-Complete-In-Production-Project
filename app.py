3fdb1e1944ad066132e3459ac4d7d7c6aad85a82
Nithesh Kumar Manimaran <53930732+Nitheshmanimaran@users.noreply.github.com>
2022-12-01 12:29:42 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import fastapi
 import uvicorn
 from fastapi import File
 from fastapi import FastAPI
@@ -7,7 +6,6 @@ import numpy as np
 import joblib
 import pandas as pd
 from pydantic import BaseModel
-import json
 import psycopg2
 from io import BytesIO
 import json
@@ -22,8 +20,6 @@ cur = conn.cursor()
 # Function to store the data in the database
 def store_data(features):
 
-    # Saving the data in the database for a preprocessed dataset
-    cur.execute('INSERT INTO processed (Airline, Source_city, Destination_city, Class, Stops, Departure_time, Arrival_time,duration,days_left,Price) VALUES (%s, %s, %s, %s, %s, %s, %s, %s,%s,%s)', (features[0], features[1], features[2], features[3], features[4], features[5], features[6], features[7], features[8], features[9]))
     
     # Replacing the encoded values with the original values before storing in the database
     features[0] = features[0].replace('0', 'Air_India').replace('1', 'AirAsia').replace('2', 'GO_FIRST').replace('3', 'Indigo').replace('4', 'SpiceJet').replace('5', 'Vistara')
@@ -34,19 +30,21 @@ def store_data(features):
     features[5] = features[5].replace('0', 'Early_Morning').replace('1', 'Morning').replace('2', 'Afternoon').replace('3', 'Evening').replace('4', 'Night').replace('5', 'Late_Night')
     features[6] = features[6].replace('0', 'Early_Morning').replace('1', 'Morning').replace('2', 'Afternoon').replace('3', 'Evening').replace('4', 'Night').replace('5', 'Late_Night')
     
-    cur.execute('INSERT INTO dataset (airline, source_city, destination_city, class, stops, departure_time, arrival_time, duration, days_left, price) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', (features[0], features[1], features[2], features[3], features[4], features[5], features[6], features[7], features[8], features[9]))
+    # Query to insert the data into the database
+    #cur.execute('INSERT INTO dataset (Airline, Source_city, Destination_city, Class, Stops, Departure_time, Arrival_time, Duration, Days_left, timestamp) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', features)
+    cur.execute('INSERT INTO dataset (airline, source_city, destination_city, class, stops, departure_time, arrival_time, duration, days_left, price,timestamp) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s)', (features[0], features[1], features[2], features[3], features[4], features[5], features[6], features[7], features[8], features[9], features[10]))
     conn.commit()
 
 # Function to get the data from the database
 def get_data(n):
-
+    # Query to get the data using timestamp
 
     cur.execute('SELECT * FROM dataset LIMIT %s', (n,))
     data = cur.fetchall()
     return data
 
 
-model = joblib.load('flight_training_model.joblib')
+model = joblib.load('flight_model.pkl')
 
 class Flight(BaseModel):
     Airline: str
@@ -68,6 +66,14 @@ def read_root():
 @app.post("/predict")
 async def predict(data : Flight):
     data = data.dict()
+    data['Airline'] = data['Airline'].replace('Air_India', '0').replace('AirAsia', '1').replace('GO_FIRST', '2').replace('Indigo', '3').replace('SpiceJet', '4').replace('Vistara', '5')
+    data['Source_city'] = data['Source_city'].replace('Bangalore', '0').replace('Chennai', '1').replace('Delhi', '2').replace('Hyderabad', '3').replace('Kolkata', '4').replace('Mumbai', '5')
+    data['Destination_city'] = data['Destination_city'].replace('Bangalore', '0').replace('Chennai', '1').replace('Delhi', '2').replace('Hyderabad', '3').replace('Kolkata', '4').replace('Mumbai', '5')
+    data['Class'] = data['Class'].replace('Business', '0').replace('Economy', '1')
+    data['Stops'] = data['Stops'].replace('zero', '0').replace('one', '1').replace('two_or_more', '2')
+    data['Departure_time'] = data['Departure_time'].replace('Early_Morning', '0').replace('Morning', '1').replace('Afternoon', '2').replace('Evening', '3').replace('Night', '4').replace('Late_Night', '5')
+    data['Arrival_time'] = data['Arrival_time'].replace('Early_Morning', '0').replace('Morning', '1').replace('Afternoon', '2').replace('Evening', '3').replace('Night', '4').replace('Late_Night', '5')
+
     Airline = data['Airline']
     Source_city = data['Source_city']
     Destination_city = data['Destination_city']
@@ -82,6 +88,7 @@ async def predict(data : Flight):
     prediction = model.predict([[Airline, Source_city, Destination_city, Class, Stops, Departure_time, Arrival_time, Duration, Days_left]])
     # appending the predicted price to features
     features.append(prediction[0])
+    features.append(pd.Timestamp.now())
 
     # Sending the features list to a postgreSQL database
     store_data(features)
@@ -93,13 +100,26 @@ async def predict(data : Flight):
         'prediction': prediction[0]
     }
 
-@app.get("/past")
-# async function to n rows of data from the database
-async def past(n : int):
-    data = get_data(n)
-    return data
+@app.get("/pastpredictions")
+def past_predictions(n: int, date1: str, time1: str, date2: str, time2: str):
+    # Converting the date and time to timestamp
+    limit = n
+    date1 = date1 + ' ' + time1
+    date2 = date2 + ' ' + time2
+    date1 = pd.Timestamp(date1)
+    date2 = pd.Timestamp(date2)
+    # Query to get the data using timestamp including date2
+    cur.execute('SELECT * FROM dataset WHERE timestamp >= %s AND timestamp <= %s LIMIT %s',(date1, date2, limit))
     
+    data = cur.fetchall()
+    # Converting the data to a dataframe
+    df = pd.DataFrame(data, columns=['Airline', 'Source_city', 'Destination_city', 'Class', 'Stops', 'Departure_time', 'Arrival_time', 'Duration', 'Days_left', 'Price', 'timestamp'])
+    # Converting the timestamp to date and time
+    df['timestamp'] = df['timestamp'].dt.strftime('%Y-%m-%d %H:%M')
+    # Converting the dataframe to a dictionary
+    data = df.to_dict('records')
 
+    return data
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
--- streamlit.py
+++ streamlit.py
@@ -1,263 +1,97 @@
-import base64
-from ctypes.wintypes import SIZE
-from io import StringIO
-import streamlit as st
-import pandas as pd 
-import numpy as np
-import datetime
-import time
-import emoji
-import warnings
 import csv
+import warnings
+from io import StringIO
+import pandas as pd
 import PIL.Image as Image
 import requests
-
-
+import streamlit as st
+import joblib
 warnings.filterwarnings('ignore')
 
+
+
+footer = """
+<style>
+footer:after {
+    content : 'Created by: Awesome DSA GROUP, ';
+    display: block;
+    position: relative;
+    padding: 5px 0px;
+    top: 3px;
+}
+.block-container {
+    padding: 1rem !important
+}
+</style>
+"""
 img = Image.open('Epita.png')
 st.image(img, width=500)
-
-# Title
+st.markdown(footer, unsafe_allow_html=True)
 st.title('Flight Price Prediction')
+st.subheader('Enter the details of the flight to get the price')
 
-# Streamlit subheader
-st.subheader('Enter the details of the flight')
+with st.expander('On Demand Prediction'):
+    # Getting input from the user
+    input_dict = {
+        'Airline': st.selectbox('Airline', ('Air_India', 'AirAsia', 'GO_FIRST', 'Indigo', 'SpiceJet', 'Vistara')),
+        'Source_city': st.selectbox('Source City', ('Bangalore', 'Chennai', 'Delhi', 'Hyderabad', 'Kolkata', 'Mumbai')),
+        'Destination_city': st.selectbox('Destination City', ('Bangalore', 'Chennai', 'Delhi', 'Hyderabad', 'Kolkata', 'Mumbai')),
+        'Class': st.radio('Class', ('Business', 'Economy')),
+        'Stops': st.radio('Stops', ('zero', 'one', 'two_or_more')),
+        'Departure_time': st.selectbox('Departure Time', ('Early_Morning', 'Morning', 'Afternoon', 'Evening', 'Night', 'Late_Night')),
+        'Arrival_time': st.selectbox('Arrival Time', ('Early_Morning', 'Morning', 'Afternoon', 'Evening', 'Night', 'Late_Night')),
+        'Duration': st.number_input('Duration in hours', min_value=0, max_value=100, value=0),
+        'Days_left': st.number_input('Days left for the flight', min_value=0, max_value=100, value=0)
+    }
 
-# Getting the input from the user for airline, source_city, destination_city, class, stops, departure_time, arrival_time, duration,days_left
-airline = st.selectbox('Airline', ('Air_India', 'AirAsia', 'GO_FIRST', 'Indigo', 'SpiceJet', 'Vistara'))
-source_city = st.selectbox('Source City', ('Bangalore', 'Chennai', 'Delhi', 'Hyderabad', 'Kolkata', 'Mumbai'))
-destination_city = st.selectbox('Destination City', ('Bangalore', 'Chennai', 'Delhi', 'Hyderabad', 'Kolkata', 'Mumbai'))
-
-# Radio button for class
-class_ = st.radio('Class', ('Business', 'Economy'))
-
-# Radio button for stops
-stops = st.radio('Stops', ('zero', 'one', 'two_or_more'))
-
-# Selectbox for departure time
-departure_time = st.selectbox('Departure Time', ('Early_Morning', 'Morning', 'Afternoon', 'Evening', 'Night', 'Late_Night'))
-
-# Selectbox for arrival time
-arrival_time = st.selectbox('Arrival Time', ('Early_Morning', 'Morning', 'Afternoon', 'Evening', 'Night', 'Late_Night'))
-
-# Input for duration Saying user to enter the duration in hours
-duration = st.number_input('Duration in hours', min_value=0, max_value=100, value=0)
-
-# Input for days left
-days_left = st.number_input('Days left for the flight', min_value=0, max_value=100, value=0)
-
-# Creating a dataframe
-df = pd.DataFrame({'Airline': [airline], 'Source_city': [source_city], 'Destination_city': [destination_city], 'Class': [class_], 'Stops': [stops], 'Departure_time': [departure_time], 'Arrival_time': [arrival_time], 'Duration': [duration], 'Days_left': [days_left]})
-
-# Converting the categorical variables to numerical variables
-df['Airline'] = df['Airline'].map({'Air_India': 0, 'AirAsia': 1, 'GO_FIRST': 2, 'Indigo': 3, 'SpiceJet': 4, 'Vistara': 5})
-df['Source_city'] = df['Source_city'].map({'Bangalore': 0, 'Chennai': 1, 'Delhi': 2, 'Hyderabad': 3, 'Kolkata': 4, 'Mumbai': 5})
-df['Destination_city'] = df['Destination_city'].map({'Bangalore': 0, 'Chennai': 1, 'Delhi': 2, 'Hyderabad': 3, 'Kolkata': 4, 'Mumbai': 5})
-df['Class'] = df['Class'].map({'Business': 0, 'Economy': 1})
-df['Stops'] = df['Stops'].map({'zero': 0, 'one': 1, 'two_or_more': 2})
-df['Departure_time'] = df['Departure_time'].map({'Early_Morning': 0, 'Morning': 1, 'Afternoon': 2, 'Evening': 3, 'Night': 4, 'Late_Night': 5})
-df['Arrival_time'] = df['Arrival_time'].map({'Early_Morning': 0, 'Morning': 1, 'Afternoon': 2, 'Evening': 3, 'Night': 4, 'Late_Night': 5})
+    # Displaying the input to the user
+    st.write('The input given by the user is:')
+    st.write(input_dict)
 
 
-# Displaying the dataframe
-st.write(df)
-# Convert the dataframe to a JSON string
-json_data = {
-    'Airline': str(df['Airline'].values[0]),
-    'Source_city': str(df['Source_city'].values[0]),
-    'Destination_city': str(df['Destination_city'].values[0]),
-    'Class': str(df['Class'].values[0]),
-    'Stops': str(df['Stops'].values[0]),
-    'Departure_time': str(df['Departure_time'].values[0]),
-    'Arrival_time': str(df['Arrival_time'].values[0]),
-    'Duration': str(df['Duration'].values[0]),
-    'Days_left': str(df['Days_left'].values[0])
-}
+    if st.button('Predict'):
+        response = requests.post('http://127.0.0.1:8000/predict', json=input_dict)
+        prediction = (response.json())
+        st.write('The price of the flight is: ', prediction['prediction'])
 
-# Send a POST request to the API
-if st.button('Predict'):
-    # Send the JSON to the API
-    response = requests.post('http://127.0.0.1:8000/predict', json=json_data)
-    # Get the prediction
-    prediction = response.json()
-    # Display the prediction
-    st.write('The predicted price of the flight is', prediction['prediction'])
 
-# Dropdown for batch prediction
-batch_prediction = st.selectbox('Batch Prediction', ('Processed CSV', 'Unprocessed CSV'))
+with st.expander('Batch Prediction'):
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        df['price']=''
 
-def processed_csv():
-    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+    if st.button('Predict Batch'):
 
-    for uploaded_file in uploaded_files:
-        bytes_data = uploaded_file.read()
-        st.write("filename:", uploaded_file.name)
-    
-    # Converting the bytes data to csv file
-        csv_file = StringIO(bytes_data.decode('utf-8'))
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
+        for i in range(len(df)):
+            input_dict = {
+                'Airline': str(df['airline'][i]),
+                'Source_city': str(df['source_city'][i]),
+                'Destination_city': str(df['destination_city'][i]),
+                'Class': str(df['class'][i]),
+                'Stops': str(df['stops'][i]),
+                'Departure_time': str(df['departure_time'][i]),
+                'Arrival_time': str(df['arrival_time'][i]),
+                'Duration': str(df['duration'][i]),
+                'Days_left': str(df['days_left'][i])
 
-        column_names=[]
-        row_values=[]
-        for row in csv_reader:
-            if line_count == 0:
-            #Storing the column names in column_names
-                column_names = row
-                line_count += 1
-            else:
-            #Storing all the rows in row_values
-                row_values.append(row)
-                line_count += 1
-        st.write(f'Processed {line_count - 1} lines.')
+            }
+            response = requests.post('http://127.0.0.1:8000/predict', json=input_dict)
+            prediction = (response.json())
+            df['price'][i] = prediction['prediction']
 
-   
-    # Displaying a table using the column names and row values
-        df = pd.DataFrame(row_values, columns=column_names)
         st.write(df)
-    
-    # For batch prediction
-        if st.button('Predict_Batch_Processed'):
-            price = []
-            for i in df.index:
 
-                json_data = {
-                    'Airline': str(df['airline'].values[i]),
-                    'Source_city': str(df['source_city'].values[i]),
-                    'Destination_city': str(df['destination_city'].values[i]),
-                    'Class': str(df['class'].values[i]),
-                    'Stops': str(df['stops'].values[i]),
-                    'Departure_time': str(df['departure_time'].values[i]),
-                    'Arrival_time': str(df['arrival_time'].values[i]),
-                    'Duration': str(df['duration'].values[i]),
-                    'Days_left': str(df['days_left'].values[i])
-                }
-                response = requests.post('http://127.0.0.1:8000/predict', json=json_data)
-                prediction = response.json()
-                
-                #st.write('The predicted price of the flight is', prediction['prediction'])
-                price.append(prediction['prediction'])
-            df['Price'] = price
-
-            st.write('The dataframe with the predicted price is')
-            st.write(df)
-
-def unprocessed_csv():
-
-
-    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-
-    for uploaded_file in uploaded_files:
-        bytes_data = uploaded_file.read()
-        st.write("filename:", uploaded_file.name)
-    
-    # Converting the bytes data to csv file
-        csv_file = StringIO(bytes_data.decode('utf-8'))
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-
-        column_names=[]
-        row_values=[]
-        for row in csv_reader:
-            if line_count == 0:
-            #Storing the column names in column_names
-                column_names = row
-                line_count += 1
-            else:
-            #Storing all the rows in row_values
-                row_values.append(row)
-                line_count += 1
-        st.write(f'Processed {line_count - 1} lines.')
-
-   
-    # Displaying a table using the column names and row values
-        df = pd.DataFrame(row_values, columns=column_names)
-        #st.write(df)
-
-    # deleting the unwanted columns from the dataframe - unnamed column with index, flight, price
-
-        df = df.drop(['index', 'flight','price'], axis=1)
-        #st.write(df)
-
-        #Creating a dictionary for the airline column in alphabetical order
-        airline_dict = {'Air_India': 0, 'AirAsia': 1, 'GO_FIRST': 2, 'Indigo': 3, 'SpiceJet': 4, 'Vistara': 5}
-        #Creating a dictionary for the source_city column in alphabetical order
-        source_city_dict = {'Bangalore': 0, 'Chennai': 1, 'Delhi': 2, 'Hyderabad': 3, 'Kolkata': 4, 'Mumbai': 5}
-        #Creating a dictionary for the destination_city column in alphabetical order
-        destination_city_dict = {'Bangalore': 0, 'Chennai': 1, 'Delhi': 2, 'Hyderabad': 3, 'Kolkata': 4, 'Mumbai': 5}
-        #Creating a dictionary for the class column in alphabetical order
-        class_dict = {'Business': 0, 'Economy': 1}
-        #Creating a dictionary for the departure_time column in alphabetical order
-        departure_time_dict = {'Early_Morning': 0, 'Morning': 1, 'Afternoon': 2, 'Evening': 3, 'Night': 4, 'Late_Night': 5}
-        #Creating a dictionary for the arrival_time column in alphabetical order
-        arrival_time_dict = {'Early_Morning': 0, 'Morning': 1, 'Afternoon': 2, 'Evening': 3, 'Night': 4, 'Late_Night': 5}
-        #Creating a dictionary for the stops column in alphabetical order
-        stops_dict = {'zero': 0, 'one': 1, 'two_or_more': 2}
-
-        #Replacing the categorical values with the corresponding numerical values
-        df['airline'] = df['airline'].map(airline_dict)
-        df['source_city'] = df['source_city'].map(source_city_dict)
-        df['destination_city'] = df['destination_city'].map(destination_city_dict)
-        df['class'] = df['class'].map(class_dict)
-        df['departure_time'] = df['departure_time'].map(departure_time_dict)
-        df['arrival_time'] = df['arrival_time'].map(arrival_time_dict)
-        df['stops'] = df['stops'].map(stops_dict)
-        #Converting the duration column to integer
-        df['duration'] = df['duration'].astype(float)
-        df['duration'] = df['duration'].astype(int)
-        df['days_left'] = df['days_left']
-        
-        st.write('The dataframe after preprocessing is:')
+with st.expander('Get Past Predictions'):
+    n = st.number_input('Enter the number of past predictions you want to see', min_value=0, max_value=100, value=0)
+    date1 = st.date_input('From')
+    time1 = st.text_input('Enter the time in HH:MM format', '00:00')
+    date2 = st.date_input('To')
+    time2 = st.text_input('Enter the time in HH:MM format', '12:00')
+  
+    if st.button('Get Past Predictions'):
+        response = requests.get('http://127.0.0.1:8000/pastpredictions', params={'n': n, 'date1': date1, 'time1': time1, 'date2': date2, 'time2': time2})
+        #Converting it intp a dataframe
+        data = response.json()
+        df = pd.DataFrame(data)
         st.write(df)
-        if st.button('Predict_Batch_Unprocessed'):
-            price = []
-            for i in df.index:
-
-                json_data = {
-                    'Airline': str(df['airline'].values[i]),
-                    'Source_city': str(df['source_city'].values[i]),
-                    'Destination_city': str(df['destination_city'].values[i]),
-                    'Class': str(df['class'].values[i]),
-                    'Stops': str(df['stops'].values[i]),
-                    'Departure_time': str(df['departure_time'].values[i]),
-                    'Arrival_time': str(df['arrival_time'].values[i]),
-                    'Duration': str(df['duration'].values[i]),
-                    'Days_left': str(df['days_left'].values[i])
-                }
-                response = requests.post('http://127.0.0.1:8000/predict', json=json_data)
-                prediction = response.json()
-                
-                #st.write('The predicted price of the flight is', prediction['prediction'])
-                price.append(prediction['prediction'])
-            df['Price'] = price
-
-            st.write('The dataframe after prediction is:')
-            st.write(df)
-
-
-
-#If batch prediction is selected as processed csv then processed_csv() function is called
-if batch_prediction == 'Processed CSV':
-    processed_csv()
-
-#If batch prediction is selected as unprocessed csv then unprocessed_csv() function is called
-elif batch_prediction == 'Unprocessed CSV':
-    unprocessed_csv()
-
-# Asking the user to enter the number of past predictions to be displayed
-n = st.number_input('Enter the number of past predictions to be displayed', min_value=1, max_value=100000)
-
-if st.button('Get Past Predictions'): 
-    
-    # Sending a GET request to the API
-    if n!=None:
-        response = requests.get(f'http://127.0.0.1:8000/past', params={'n': n})
-        # Getting the predictions
-        predictions = response.json()
-        # Displaying the predictions as a table
-        st.write(pd.DataFrame(predictions))
-
-
-
-    
